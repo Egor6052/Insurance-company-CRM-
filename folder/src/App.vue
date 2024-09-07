022b370b8614670530333e40cbd55a128d39846e
Yegor Semenov <segor6052@gmail.com>
2024-09-07 19:25:02 +0300

    Add preparation for project

---
+++ folder/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+
+  <HomePage msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HomePage
+  }
+}
+</script>
+
+<style>
+
+   @import "./assets/css/App.css";
+</style>
---
+++ folder/src/components/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+ <div class="MainFolder">
+    <div class="logo">
+      <ul>
+        <li> <div class="logoName"><p>[ Страхова компанія ]</p></div> </li>
+        <li><div class="description"><p>Stracture FigJam</p></div></li>
+        <li><div class="Aboutdesigner"><a href=""><p>Head Desiner: Darina Dzetsyno</p></a></div>
+        </li>
+      </ul>
+    </div>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+ 
+ @import "../assets/css/HomePage.css";
+</style>
